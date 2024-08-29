b1163bb60ed7917c016e57cc49a2d3f4dbdfcf52
Steven <stevenlin3263@gmail.com>
2024-08-29 16:42:04 +0800

    Fix: 修改 Script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/<REPO>/',
   server: {
     host: '0.0.0.0',  // 监听所有 IP 地址
     port: 5173,       // 保证端口为 5173
