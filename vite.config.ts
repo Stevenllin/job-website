cf1f37a513f0fc2020d656e569b0301892186b80
Steven <stevenlin3263@gmail.com>
2024-08-30 14:03:29 +0800

    Fix: 修改 vite 中 require not defined 錯誤

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import commonjs from '@rollup/plugin-commonjs'
+import vitePluginRequire from 'vite-plugin-require';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
     host: '0.0.0.0',  // 监听所有 IP 地址
     port: 5173,       // 保证端口为 5173
   },
-  plugins: [react()],
+  plugins: [react(), vitePluginRequire.default()],
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
